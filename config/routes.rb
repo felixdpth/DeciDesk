68bc696d2b05e852fda7cfd60e534ae9cd8d1c5e
Anna Naydenova <anna.naydenova@gmail.com>
2021-02-02 21:19:27 +0100

    new routes + expenditures added

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,26 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :report
-
-  resource :treasury, only: :show do
+  resource :report do
     resources :comments, only: [:new, :create]
   end
 
-  resource :sales, only: :show do
-    resources :comments, only: [:new, :create]
-  end
+  get '/expenditure', to: 'expenditures#show'
+  get '/sales', to: 'sales#show'
+  get '/treasury', to: 'treasuries#show'
+  # resource :treasury, only: :show do
+  #   resources :comments, only: [:new, :create]
+  # end
 
-  resource :expenditure, only: :show do
-    resources :comments, only: [:new, :create]
-  end
+  # resource :sales, only: :show do
+  #   resources :comments, only: [:new, :create]
+  # end
 
-  resources :comments, only: [:edit, :update, :destroy]
+  # resource :expenditure, only: :show do
+  #   resources :comments, only: [:new, :create]
+  # end
+
+resources :comments, only: [:edit, :update, :destroy]
   # do
   #   resources :comments
   #   resources :lines
