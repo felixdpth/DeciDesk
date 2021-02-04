1cc17e1264560bd252ddd724c897d55d29a9b6da
Felix de Perthuis <fdeperthuis@sparkling-partners.com>
2021-02-04 21:30:26 +0100

    treasury dash

--- app/models/line.rb
+++ app/models/line.rb
@@ -3,16 +3,15 @@ class Line < ApplicationRecord
 
   ##Treasury
   scope :treasury, -> { where(category: "Treasury") }
-  scope :treasury_debit, -> { where(credit: "0", category: "Treasury") }
-  scope :treasury_credit, -> { where(credit: "Credit", category: "Treasury") }
+  scope :treasury_debit, -> { where(credit: "0", category: "Treasury").pluck(:debit) }
+  scope :treasury_credit, -> { where(debit: "0", category: "Treasury").pluck(:credit) }
   scope :treasury_debit_date, ->(date) { treasury_debit.where("ecriture_date < ?", date) }
-  scope :treasury_credit_date
-  
-  def self.all_debit(date)
-    treasury_debit_date(date).sum(:debit)
-  end
 
-  def self.treasury_debit_date(date)
-    treasury_debit.where("ecriture_date < ?", date)
-  end
+    # def self.all_debit(date)
+    #   treasury_debit_date(date).sum(:debit)
+    # end
+
+    # def self.treasury_debit_date(date)
+    #   treasury_debit.where("ecriture_date < ?", date)
+    # end
 end
