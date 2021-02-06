9040f3177afc9191dc0876f2277240e8a4323138
Felix de Perthuis <fdeperthuis@sparkling-partners.com>
2021-02-06 15:58:08 +0100

    chart and table

--- app/controllers/treasuries_controller.rb
+++ app/controllers/treasuries_controller.rb
@@ -12,5 +12,9 @@ class TreasuriesController < ApplicationController
     authorize current_user, policy_class: TreasuriesPolicy
     @top5_debit = @lines.treasury_top5_debit
     @top5_credit = @lines.treasury_top5_credit
+    @treasury_last_day = @lines.treasury_last_day
+    @cash_in = @lines.treasury_debit_lines.sum
+    @cash_out = @lines.treasury_credit_lines.sum
+    @balance_last_day = @cash_in - @cash_out
   end
 end
