256fb1bf0e485fc62418cbc5f42eae45471e624f
Anna Naydenova <anna.naydenova@gmail.com>
2021-02-06 16:07:35 +0100

    expenditures updated

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -29,3 +29,16 @@ class ExpendituresController < ApplicationController
     authorize current_user, policy_class: ExpenditurePolicy
   end
 end
+
+
+
+# class SalesController < ApplicationController
+#   def show
+#     @report = Report.find params[:report_id]
+#     @sales = @report.lines.where(category: "Sales")
+#     authorize current_user, policy_class: SalePolicy
+#     @top5sales_credit = @report.sales_top5sales_credit
+#     @sales_volume = @sales.group_by_month(:ecriture_date).count
+#     @annual_sales = @report.annual_sales
+#   end
+# end
