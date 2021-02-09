62891b888410db29df510ab6c60ec7e21291cd6d
Anna Naydenova <anna.naydenova@gmail.com>
2021-02-09 20:38:33 +0100

    transactions added

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,22 +1,6 @@
 class ExpendituresController < ApplicationController
-#   before_action :authenticate_user!
-#   before_action :authorize_user
+before_action :set_report, only: [:show, :transactions]
 
-#   def show
-#   end
-
-#   def advice
-#   end
-
-#   def comments
-#   end
-
-#   private
-
-#   def authorize_user
-#     authorize :expenditure, :expenditure?
-#   end
-# end
   def index
     @lines = Line.all
     @expenditures = Line.expenditure
@@ -25,9 +9,33 @@ class ExpendituresController < ApplicationController
   end
 
   def show
-    @report = Report.find params[:report_id]
     @expenditures = @report.lines.where(category: "Expenditures")
     authorize current_user, policy_class: ExpenditurePolicy
     @top5_expenditures = @report.expenditures_top5_debit
+  end
+
+  def transactions
+    authorize current_user, policy_class: ExpenditurePolicy
+    @lines = @report.lines.where(category: "Expenditures")
+    if params[:query].present?
+      sql_query = "compte_num ILIKE :query OR ecriture_lib ILIKE :query OR debit ILIKE :query OR credit ILIKE :query"
+      @lines = @lines.where(sql_query, query: "%#{params[:query]}%")
+    end
+  end
+
+  def advice
+  end
+
+  def comments
+  end
+
+  private
+
+  def transactions_params
+    params.require(:lines).permit(:ecriture_lib, :ecriture_date, :compte_num, :debit, :credit, :compte_lib)
+  end
+
+  def set_report
+    @report = Report.find params[:report_id]
   end
 end
